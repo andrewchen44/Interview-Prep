b04c20696163461bff0d44511958a8dc6c71a378
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-24 15:38:02 -0800

    Add comments

--- Cracking the Coding Interview/Math and Logic Puzzles/Sieve of Eratosthenes.js
+++ Cracking the Coding Interview/Math and Logic Puzzles/Sieve of Eratosthenes.js
@@ -1,5 +1,6 @@
  const sieveOfEratosthenes = function (max) {
     let nums = [];
+    // Initiate numbers up to max
     for(let i = 0; i < max; i++) {
         nums.push(i);
     }
@@ -7,6 +8,7 @@
     nums[1] = false;
     let prime = 2;
 
+    // Keep removing multiples of price until the prime number is greater than the square root of the max value
     while(prime <= Math.sqrt(max)) {
         removeMultiples(nums, prime);
         prime = getNextPrime(nums, prime);
@@ -14,6 +16,7 @@
 
     let results = [];
 
+    // Populate new array with numbers only
     for(let i = 0; i < nums.length; i++) {
         if(nums[i]) {
             results.push(nums[i])
