3c92ec4cde0b760196f7f8fb8bcfa20af2a3b60e
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-24 11:46:14 -0800

    Create swapOddEvenBits function

---
+++ Cracking the Coding Interview/Bit Manipulation/5.7 Pairwise Swap.js
@@ -0,0 +1,8 @@
+// write a function that swaps odd and even bits in an integer with as few instructions as possible.
+
+const swapOddEvenBits = function (num) {
+    return ( (num & 0xaaaaaaaa) >>> 1) | ((num & 0x55555555) << 1)
+}
+
+// 0xA: pattern that creates 10101010 to get odd bits
+// 0x5: pattern that creates 01010101 to get even bits
