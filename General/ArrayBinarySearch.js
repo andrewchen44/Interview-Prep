6ed0be4a3b829a21cd4f118906470a4b1449c114
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-24 22:35:50 -0800

    Implement binary search function for a sorted array

---
+++ General/ArrayBinarySearch.js
@@ -0,0 +1,22 @@
+// return index of search value if in the sorted array, -1 otherwise
+
+const binarySearch = function (arr, value) {
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low <= high) {
+        let mid = (low + high) / 2 | 0;
+        let midVal = arr[mid];
+        if(midVal < value) {
+            low = mid + 1;
+        } else if(midVal > value) {
+            high = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return -1
+}
+
+// Keeps comparing the middle of each section to the value until it is found
