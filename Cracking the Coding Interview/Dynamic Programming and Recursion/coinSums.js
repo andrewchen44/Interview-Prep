c3d9ac9a6227dfbf29bcd9b02fc92c9f8b7195d4
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-31 13:40:28 -0800

    Implement merge sort pseudocode

--- Cracking the Coding Interview/Dynamic Programming and Recursion/coinSums.js
+++ Cracking the Coding Interview/Dynamic Programming and Recursion/coinSums.js
@@ -18,4 +18,22 @@ const getSum = function(amount) {
 
   calculate(amount, 0);
   return ways;
+}const getSum = function(amount) {
+  let ways = 0;
+  const denoms = [25, 10, 5, 1];
+
+  const calculate = function(amount, index) {
+    if(index >= denoms.length - 1) {
+      ways++;
+      return;
+    }
+    currentDenom = denoms[index];
+    for(let i = 0; i * currentDenom <= amount; i++){
+      remainingAmount = amount - i * currentDenom;
+      calculate(remainingAmount, index + 1 )
+    }
+  }
+
+  calculate(amount, 0);
+  return ways;
 }
---
+++ Cracking the Coding Interview/Sorting/mergeSort.js
@@ -0,0 +1,19 @@
+// Implement merge sort
+
+
+
+// merge sort function takes in array
+  // if array is null
+    // return
+
+  // set left half to variable
+  // set right half to variable
+  
+  // call merge sort to left half
+  // call merge sort to right half
+
+  //call merge halves
+
+
+
+// merge halves function
