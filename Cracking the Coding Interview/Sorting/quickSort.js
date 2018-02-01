764aa318ba0658d0e0d3f7d48be860f7d0d30c7e
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-31 16:55:59 -0800

    Add descriptive comments

--- Cracking the Coding Interview/Sorting/quickSort.js
+++ Cracking the Coding Interview/Sorting/quickSort.js
@@ -1,17 +1,17 @@
 // Implement quicksort for funsies
 
 const quickSort = function(array, left, right){
+  //sets the initial left right value
   if(left === undefined && right === undefined) {
     left = 0;
     right = array.length - 1;
   }
+  // only recurses if left value is less than right
   if(left < right) {
-    
     let partitionIndex = partition(array, left, right);
     quickSort(array, left, partitionIndex - 1);
     quickSort(array, partitionIndex, right);
   }
-
   return array
 }
 
@@ -19,6 +19,7 @@ const partition = function(array, left, right) {
   let pivotValue = array[Math.floor((right + left) / 2)];
   let l = left;
   let r = right;
+  // goes through array and swaps values if larger than pivot
   while(l <= r) {
     while(array[l] < pivotValue){
       l++;
