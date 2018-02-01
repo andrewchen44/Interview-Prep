650202d6ef6b2ed7db1e81a8329e7f947b1ce5ef
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-31 16:52:41 -0800

    Implement quick sort tested

--- Cracking the Coding Interview/Sorting/quickSort.js
+++ Cracking the Coding Interview/Sorting/quickSort.js
@@ -5,21 +5,19 @@ const quickSort = function(array, left, right){
     left = 0;
     right = array.length - 1;
   }
-  if(array.length === 1) {
-    return arr;
+  // debugger;
+  if(left < right) {
+    
+    let partitionIndex = partition(array, left, right);
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex, right);
   }
-  let pivot = Math.floor((right - left)/2);
-
-  let partitionIndex = partition(array, pivot, left, right);
-  quickSort(array, left, partitionIndex - 1);
-  quickSort(array, partitionIndex + 1, right);
-
 
   return array
 }
 
-const partition = function(array, pivot, left, right) {
-  let pivotValue = array[pivot];
+const partition = function(array, left, right) {
+  let pivotValue = array[Math.floor((right + left) / 2)];
   let l = left;
   let r = right;
   while(l <= r) {
@@ -29,10 +27,10 @@ const partition = function(array, pivot, left, right) {
     while(array[r] > pivotValue) {
       r--;
     }
-    if(array[l] > pivotValue && array[r] < pivoteValue){
+    if(l <= r){
       swap(array, l, r);
       l++;
-      i--;
+      r--;
     }
   }
   return l;
@@ -43,15 +41,3 @@ const swap = function(array, left, right) {
   array[left] = array[right];
   array[right] = temp;
 }
-
-//take in array
-//if the array length is 1, return the array
-// choose the middle value as pivot
-//call comparator on array with pivit
-// call quicksort on both halves
-
-
-// comparator function that s go throuogh and swaps values
-
-
-// swap helper function
