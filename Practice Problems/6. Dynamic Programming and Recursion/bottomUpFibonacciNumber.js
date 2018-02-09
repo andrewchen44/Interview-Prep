f4f01a13644deb628fe7cbcd925e28569d9ecd04
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-09 15:02:30 -0800

    Implement climbing stairs dp

--- Practice Problems/6. Dynamic Programming and Recursion/bottomUpFibonacciNumber.js
+++ Practice Problems/6. Dynamic Programming and Recursion/bottomUpFibonacciNumber.js
@@ -2,10 +2,8 @@
 
 const fibonacci = function(n){
     let memo = [];
-
     memo[0] = 0;
     memo[1] = 1;
-
     if(n === 1 || n === 0) {
         return memo[n];
     } else {
@@ -13,8 +11,29 @@ const fibonacci = function(n){
             memo[i] = memo[i - 2] + memo[i - 1];
         }
     }
-
     return memo[n - 1]
 }
 
 // calculate all the numbers and add them to an array, then simply return the number array position n - 1
+// n: which fibonacci number desired
+// Time Complexity: O(n) as you need to calculate every fibonacci number before it.
+// Space complexity: O(n) as you need to store a value for each value before it
+
+// Fibonacci with O(1) space complexity below by only keeping track of previous two values
+
+const fibonacci = function(n) {
+  let memo = [];
+  two = 0;
+  one = 1;
+  if (n === 1 || n === 0) {
+    return n;
+  } else {
+    for (let i = 2; i < n; i++) {
+      let current = two + one;
+      two = one;
+      one = current
+    }
+  }
+  return one
+};
+
--- Practice Problems/6. Dynamic Programming and Recursion/climbingStairs.js
+++ Practice Problems/6. Dynamic Programming and Recursion/climbingStairs.js
@@ -1,4 +1,4 @@
-// Create a function that determines how many waysy ou can climb n stairs if you can only take 1 or two steps at a time
+// Create a function that determines how many ways you can climb n stairs if you can only take 1 or two steps at a time
 
 function climbingStairs(n) {
     let memo = [];
@@ -16,4 +16,19 @@ function climbingStairs(n) {
         }
     }
     return climb(n);
+}
+
+// n: number of stairs
+// Time Complexity: O(n) as we need to calculate how any ways to climb all stairs before it
+// Space Complexity: O(n) as we need to store all calculated values of stairs before it
+
+function climbingStairs(n) {
+    let dp = [];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(let i = 3; i <= n; i++) {
+        dp[i] = dp[i-2] + dp[i-1];
+    }
+    return dp(n);
 }
